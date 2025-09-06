de329ac77bc748b7e34422af58606365af49af77
Askinga <musicgemca@gmail.com>
2025-09-06 19:56:40 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("f", {
-    name: "Flowers", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "Effortlessless", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "D-1", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,9 +8,9 @@ addLayer("f", {
 		best: new ExpantaNum(0),
 		total: new ExpantaNum(0),
     }},
-    color: "#F4ACA1",
+    color: "#ccffff",
     requires: new ExpantaNum(10), // Can be a function that takes requirement increases into account
-    resource: "Flowers", // Name of prestige currency
+    resource: "Effortlessless", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -24,13 +24,16 @@ addLayer("f", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "f", description: "F: Reset for flowers", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "e", description: "E: Reset for effortlessless", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+	automate(){
+		player.best = player.points.max(player.points, player.best)
+	},
     layerShown(){return true},
     upgrades: {
 	11: {
-		title: "More Bees",
-		description: "x2 Bee gain.",
+		title: "1",
+		description: "x2 Point gain.",
 		cost: new ExpantaNum(1)
 	},
     },		
