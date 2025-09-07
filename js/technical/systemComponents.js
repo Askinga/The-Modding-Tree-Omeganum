d51efb5c471426ec97e0e9090d2147558f9a3076
Askinga <musicgemca@gmail.com>
2025-09-06 22:59:16 -0400

    Update systemComponents.js

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -134,6 +134,8 @@ var systemComponents = {
         The Modding Tree <a v-bind:href="'https://github.com/Acamaeda/The-Modding-Tree/blob/master/changelog.md'" target="_blank" class="link" v-bind:style = "{'font-size': '14px', 'display': 'inline'}" >{{TMT_VERSION.tmtNum}}</a> by Acamaeda
         <br>
         The Prestige Tree made by Jacorb and Aarex
+		<br>
+        The Modding Tree OmegaNum made by unsoftcapped3
 		<br><br>
 		<div class="link" onclick="showTab('changelog-tab')">Changelog</div><br>
         <span v-if="modInfo.discordLink"><a class="link" v-bind:href="modInfo.discordLink" target="_blank">{{modInfo.discordName}}</a><br></span>
