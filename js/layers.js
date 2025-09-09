ebc890cbd4dee958383a58e58719ed3f3507abf1
Askinga <musicgemca@gmail.com>
2025-09-09 07:29:55 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,6 +37,10 @@ addLayer("f", {
 			alert("Sorry! No Dev Speed in here! I want you to experience the TRUE difficulty of the tree! (it is a difficulty chart tree!)")
 			player.devSpeed = new OmegaNum(1)
 		}
+		if (player.devSpeed === "undefined") {
+			alert("Oops! Looks like you broke my game. I will fix it for you!")
+			player.devSpeed = new OmegaNum(1)
+		}
     },
     layerShown(){return true},
     upgrades: {
