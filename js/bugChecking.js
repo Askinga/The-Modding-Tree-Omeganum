282846ff416a62a68f3461f6da0aa679dcef20b8
Askinga <musicgemca@gmail.com>
2025-09-09 08:04:23 -0400

    Update bugChecking.js

--- js/bugChecking.js
+++ js/bugChecking.js
@@ -5,7 +5,7 @@ function bugChecks() {
 			alert("Oops! Looks like you broke my game. I will fix it for you!")
 			player.devSpeed = new OmegaNum(1)
 		}
-		if (player.devSpeed.gt(1)) {
+		if (player.devSpeed.gte(1.000000001)) {
 			alert("Sorry! No Dev Speed in here! I want you to experience the TRUE difficulty of the tree! (it is a difficulty chart tree!)")
 			player.devSpeed = new OmegaNum(1)
 		}
