cc2cc938a529c094f9cc07c7e42888abb20a0586
Askinga <musicgemca@gmail.com>
2025-09-09 07:43:46 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -32,16 +32,6 @@ addLayer("f", {
     hotkeys: [
         {key: "e", description: "E: Reset for effortlessless", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-	automate(){
-		if (player.devSpeed.gt(1)) {
-			alert("Sorry! No Dev Speed in here! I want you to experience the TRUE difficulty of the tree! (it is a difficulty chart tree!)")
-			player.devSpeed = new OmegaNum(1)
-		}
-		if (player.devSpeed === undefined) {
-			alert("Oops! Looks like you broke my game. I will fix it for you!")
-			player.devSpeed = new OmegaNum(1)
-		}
-    },
     layerShown(){return true},
     upgrades: {
 	11: {
