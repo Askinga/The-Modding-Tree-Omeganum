43d1f8e9dd13a6665b8a050dcc46e0fca8a8523d
Askinga <musicgemca@gmail.com>
2025-09-09 07:49:08 -0400

    Update bugChecking.js

--- js/bugChecking.js
+++ js/bugChecking.js
@@ -1,12 +1,12 @@
-setInterval(bugChecks(), 1000)
+setInterval(bugChecks, 1000)
 
 function bugChecks() {
-		if (player.devSpeed.gt(1)) {
-			alert("Sorry! No Dev Speed in here! I want you to experience the TRUE difficulty of the tree! (it is a difficulty chart tree!)")
+	    if (player.devSpeed === undefined) {
+			alert("Oops! Looks like you broke my game. I will fix it for you!")
 			player.devSpeed = new OmegaNum(1)
 		}
-		if (player.devSpeed === undefined) {
-			alert("Oops! Looks like you broke my game. I will fix it for you!")
+		if (player.devSpeed.gt(1)) {
+			alert("Sorry! No Dev Speed in here! I want you to experience the TRUE difficulty of the tree! (it is a difficulty chart tree!)")
 			player.devSpeed = new OmegaNum(1)
 		}
 }
