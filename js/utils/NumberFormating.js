7b6bdcecd8d5e9654242b98124b5a9e4e4daf692
Askinga <musicgemca@gmail.com>
2025-09-22 18:20:16 -0400

    Update NumberFormating.js

--- js/utils/NumberFormating.js
+++ js/utils/NumberFormating.js
@@ -1,9 +1,9 @@
 
 function exponentialFormat(num, precision, mantissa = true) {
     let e = num.log10().floor()
-    let m = num.div(ExpantaNum.pow(10, e))
+    let m = num.div(OmegaNum.pow(10, e))
     if (m.toStringWithDecimalPlaces(precision) == 10) {
-        m = ExpantaNumOne
+        m = OmegaNumOne
         e = e.add(1)
     }
     e = (e.gte(1e9) ? format(e, 3) : (e.gte(10000) ? commaFormat(e, 0) : e.toStringWithDecimalPlaces(0)))
